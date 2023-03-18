16317adfed09ee33e97450090df80a577e1d37cd
Nicolas Camporeale <nicolase1997@gmail.com>
2023-03-18 17:55:49 -0300

    correcciones varias y centrado de botones

--- cv.js
+++ cv.js
@@ -1,4 +1,21 @@
 
-document.getElementById('boton').onclick = function(){
-    alert('dejar mensaje en mis redes');
+document.getElementById('boton1').onclick = function(){
+    alert('Para contactarme dejar mensaje en alguna de mis redes');
+}
+
+
+document.getElementById('boton2').onclick = function(){
+    location.href ='https://www.google.com/';
+}
+
+document.getElementById('boton3').onclick = function(){
+    location.href ='https://store.steampowered.com/?l=spanish';
+}
+
+document.getElementById('boton4').onclick = function(){
+    location.href ='https://web.whatsapp.com/';
+}
+
+document.getElementById('boton5').onclick = function(){
+    location.href ='https://pc-builds.com/es/';
 }
